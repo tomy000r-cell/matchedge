67720ad7825e5fb88114a5691fa8afa06d2d4e0e
tomy000r-cell <to.my000r@gmail.com>
2026-02-16 20:11:27 +0100

    apiFootball.ts

--- lib/apiFootball.ts
+++ lib/apiFootball.ts
@@ -1,3 +1,7 @@
-export const getHeaders = () => ({
-  "x-apisports-key": process.env.FOOTBALL_API_KEY!,
-});
+export const getHeaders = () => {
+  console.log("KEY:", process.env.FOOTBALL_API_KEY);
+  
+  return {
+    "x-apisports-key": process.env.FOOTBALL_API_KEY || "",
+  };
+};
