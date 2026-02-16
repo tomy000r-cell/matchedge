a68a281d63b678a64c0881852305912463afa567
tomy000r-cell <to.my000r@gmail.com>
2026-02-16 20:06:04 +0100

    apiFootball.ts

--- lib/apiFootball.ts
+++ lib/apiFootball.ts
@@ -1,5 +1,3 @@
-export const BASE_URL = "https://v3.football.api-sports.io/";
-
 export const getHeaders = () => ({
-  "x-apisports-key": process.env.API_FOOTBALL_KEY!,
+  "x-apisports-key": process.env.FOOTBALL_API_KEY!,
 });
