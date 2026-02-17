e582d1f1a1faf08f21db142ec11dbbef8e5d564b
tomy000r-cell <to.my000r@gmail.com>
2026-02-17 09:39:03 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,16 @@
-import Link from “next/link”;
-import React from “react”;
-
-export default function RootLayout(props: { children: React.ReactNode }) {
-return (
-
-<body style={{ margin: 0 }}>
-<nav
-style={{
-display: “flex”,
-gap: 20,
-padding: 20,
-borderBottom: “1px solid #ddd”
-}}
-
-Accueil
-Matchs
-Classement
-Équipes
-  <div>{props.children}</div>
-  </body>
+<html>
+<body>
+<nav>
+<Link href="/">Accueil</Link>
+{" | "}
+<Link href="/matchs">Matchs</Link>
+{" | "}
+<Link href="/classement">Classement</Link>
+{" | "}
+<Link href="/equipes">Equipes</Link>
+</nav>
+{props.children}
+</body>
 </html>
-  );
+)
 }
