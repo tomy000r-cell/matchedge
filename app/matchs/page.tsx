af16f5996cd1ffe6d792bf1a94b696e25d180d35
tomy000r-cell <to.my000r@gmail.com>
2026-02-17 09:02:48 +0100

    page.tsx

--- app/matchs/page.tsx
+++ app/matchs/page.tsx
@@ -34,42 +34,44 @@ return <p style={{ padding: “20px” }}>Chargement…;
 return (
 <div style={{ padding: “20px” }}>
 Matchs Ligue 1
-{matchs.length === 0 && <p>Aucun match trouvé</p>}
+  {matchs.length === 0 && <p>Aucun match trouvé</p>}
 
-  {matchs.map((match) => (
-    <div
-      key={match.fixture.id}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: "20px",
-        padding: "10px",
-        border: "1px solid #ccc",
-        borderRadius: "8px"
-      }}
-    >
-      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-        <img
-          src={match.teams.home.logo}
-          alt={match.teams.home.name}
-          width="40"
-        />
-        <span>{match.teams.home.name}</span>
+  {matchs.map(function(match) {
+    return (
+      <div
+        key={match.fixture.id}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+          padding: "10px",
+          border: "1px solid #ccc",
+          borderRadius: "8px"
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          <img
+            src={match.teams.home.logo}
+            alt={match.teams.home.name}
+            width="40"
+          />
+          <span>{match.teams.home.name}</span>
+        </div>
+
+        <strong>VS</strong>
+
+        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          <img
+            src={match.teams.away.logo}
+            alt={match.teams.away.name}
+            width="40"
+          />
+          <span>{match.teams.away.name}</span>
+        </div>
       </div>
-
-      <strong>VS</strong>
-
-      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-        <img
-          src={match.teams.away.logo}
-          alt={match.teams.away.name}
-          width="40"
-        />
-        <span>{match.teams.away.name}</span>
-      </div>
-    </div>
-  ))}
+    );
+  })}
 </div>
   );
 }
