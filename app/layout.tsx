26554a5faec418fa59b59e5a105a4035fb8bda50
tomy000r-cell <to.my000r@gmail.com>
2026-02-17 10:07:58 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,49 @@
-<html>
-<body>
-<nav>
-<Link href="/">Accueil</Link>
-{" | "}
-<Link href="/matchs">Matchs</Link>
-{" | "}
-<Link href="/classement">Classement</Link>
-{" | "}
-<Link href="/equipes">Equipes</Link>
-</nav>
-{props.children}
-</body>
-</html>
-)
+import "../styles/matchedge.css"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>
+
+        <div className="navbar">
+          <div className="navbar-logo">
+            MATCHEDGE
+          </div>
+
+          <div className="navbar-menu">
+            <div className="navbar-item">
+              Football
+              <div className="dropdown-content">
+                <a href="#">Pays</a>
+                <a href="#">Saisons</a>
+                <a href="#">Ligues</a>
+                <a href="#">Classement</a>
+                <a href="#">Équipes</a>
+                <a href="#">Live</a>
+                <a href="#">H2H</a>
+                <a href="#">Blessures</a>
+                <a href="#">Transferts</a>
+                <a href="#">Cotes</a>
+                <a href="#">Stats</a>
+              </div>
+            </div>
+
+            <div className="navbar-item">Prédictions</div>
+            <div className="navbar-item">Premium</div>
+            <div className="navbar-item">Bot IA</div>
+            <div className="navbar-item">Community</div>
+            <div className="navbar-item">Dashboard</div>
+            <div className="navbar-item">Connexion</div>
+          </div>
+        </div>
+
+        {children}
+
+      </body>
+    </html>
+  )
 }
