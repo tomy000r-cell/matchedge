8c3688a0b1fc760334b42f884f23afc72c6072f6
tomy000r-cell <to.my000r@gmail.com>
2026-02-17 08:06:11 +0100

    apiFootball.ts

--- lib/apiFootball.ts
+++ lib/apiFootball.ts
@@ -1,5 +1,11 @@
-export const BASE_URL = "https://v3.football.api-sports.io/";
+export function getHeaders() {
+  const apiKey = process.env.API_FOOTBALL_KEY;
 
-export const getHeaders = () => ({
-  "x-apisports-key": process.env.NEXT_PUBLIC_API_FOOTBALL_KEY,
-});
+  if (!apiKey) {
+    throw new Error("API_FOOTBALL_KEY manquante");
+  }
+
+  return {
+    "x-apisports-key": apiKey,
+  };
+}
