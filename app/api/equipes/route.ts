71ab46536fbcf047528a264d0bf12c3d97376e90
tomy000r-cell <to.my000r@gmail.com>
2026-02-17 08:33:35 +0100

    route.ts

--- app/api/equipes/route.ts
+++ app/api/equipes/route.ts
@@ -1,27 +1,17 @@
 import { NextResponse } from "next/server";
-import { BASE_URL, getHeaders } from "@/lib/apiFootball";
+import { fetchFromApi } from "@/lib/apiFootball";
 
 export async function GET() {
   try {
-    const leagueId = 61;
-    const season = 2024;
-
-    const url =
-      BASE_URL +
-      "teams?league=" +
-      leagueId +
-      "&season=" +
-      season;
-
-    const response = await fetch(url, {
-      headers: getHeaders(),
-      next: { revalidate: 86400 }, // cache 24h
-    });
-
-    const data = await response.json();
+    const data = await fetchFromApi(
+      "/teams?league=61&season=2023"
+    );
 
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error: "Erreur serveur" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Erreur récupération équipes" },
+      { status: 500 }
+    );
   }
 }
