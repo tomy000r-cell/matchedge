0352dc2ee88505b4772c309e64bcbcec57315d03
tomy000r-cell <to.my000r@gmail.com>
2026-02-17 08:31:40 +0100

    route.ts

--- app/api/standings/route.ts
+++ app/api/standings/route.ts
@@ -1,22 +1,16 @@
 import { NextResponse } from "next/server";
-import { BASE_URL, getHeaders } from "@/lib/apiFootball";
+import { fetchFromApi } from "@/lib/apiFootball";
 
 export async function GET() {
   try {
-    const response = await fetch(
-  BASE_URL + "/standings?league=61&season=2024",
-      {
-        headers: getHeaders(),
-        next: { revalidate: 3600 },
-      }
+    const data = await fetchFromApi(
+      "/standings?league=61&season=2023"
     );
 
-    const data = await response.json();
-
-    return NextResponse.json(data.response);
+    return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json(
-      { error: "Erreur récupération classement" },
+      { error: "Erreur récupération standings" },
       { status: 500 }
     );
   }
